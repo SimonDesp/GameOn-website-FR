f0b7f970caf152a2f712cc97d855b058e2030215
simon desplan <simon.desplan@gmail.com>
2023-03-15 11:42:22 +0100

    fix : close : modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const closeSpan = document.querySelectorAll(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +22,23 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event with (X)
+closeSpan.forEach((span) => span.addEventListener("click", closeModal));
 
+// faire avec echap et clic en dehors de la zone
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// validate form
+const form = document.querySelector(form);
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const number = document.getElementById("quantity")
+
+const checkName = function (name) {
+  const nameRegex = /^[a-zA-Z]{2,..}$/;
+}
